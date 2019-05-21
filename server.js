9228726618143be11cccc3fda024a3db7161cdb6
BDelleman <48587856+BDelleman@users.noreply.github.com>
2019-05-21 11:12:04 +0200

    met if statement

--- server.js
+++ server.js
@@ -62,10 +62,14 @@ app.post('/upload/photo', upload.single('myImage'), (req, res) => {
 });
 
 var appEnv = cfenv.getAppEnv(); //build URL after being assigned a Route
-const Mongo = "https://Mongo-API-";
+const Mongo = "https://Mongo-API-watson";
 const Domein = ".eu-gb.mybluemix.net";
 const path = "/post";
-var Toolchain = appEnv.app.application_name.split("-")[1,2];
+var Toolchain = appEnv.app.application_name.split("-")[2];
+if (Toolchain == undefined){
+    Toolchain = '';
+};
+
 var mongoAPIURL = Mongo.concat(Toolchain,Domein,path);
 
 app.listen(port, () => console.log(('Server started on port %d'), port));
